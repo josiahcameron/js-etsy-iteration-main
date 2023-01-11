8b159bb343e55c42761b7e6009add07aa84de10d
josiahcameron <josiahcameron2015@gmail.com>
2023-01-10 21:52:38 -0500

    Exercise 3

--- scripts/main.js
+++ scripts/main.js
@@ -19,15 +19,26 @@
     const priceAvg = (priceSum/allUsdPrices.length).toFixed(2);
     
     console.log(priceAvg)
-    ;
+    
     
     //2. Show me how to get an array of items that cost between $14.00 and $18.00 USD. Please console.log the answer.
     const limitRange = items.filter(function(item){
         return (item.price >= 14 && item.price <= 18) && item.currency_code === 'USD';
     }).map(function(item){
-         return item.title
+         return `"Items that cost between $14.00 USD and $18.00 USD:"
+          ${item.title}`
         })
     console.log(limitRange)
+
+    //3. Show me how find the item with a "GBP" currency code and print its name and price. Please console.log the one you find.
+    const onlyGBP = items.filter(function(item){
+        return item.currency_code === 'GBP';
+    }).map(function(item){
+        const gbpName = item.title;
+        const gbpPrice = item.price;
+        return `${gbpName} costs £${gbpPrice}.`
+    })
+    console.log(onlyGBP);
 
 
 
