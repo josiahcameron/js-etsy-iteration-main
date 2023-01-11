ad1fb6ce2761c5a6272e8fe5593097715f893044
josiahcameron <josiahcameron2015@gmail.com>
2023-01-10 19:01:01 -0500

    Exercise 2

--- scripts/main.js
+++ scripts/main.js
@@ -2,27 +2,32 @@
     'use strict';
 
     //1. Show me how to calculate the average price of all items listed in USD. Please console.log the average.
+
+    
     const onlyUSD = items.filter(function(item){
         return item.currency_code === 'USD';
     })
 
-    const allPrices = onlyUSD.map(function(item){
+    const allUsdPrices = onlyUSD.map(function(item){
         return item.price;
         })
 
-    const priceSum = allPrices.reduce(function(total,current){
+    const priceSum = allUsdPrices.reduce(function(total,current){
         return total + current;
     })
 
-    const priceAvg = (priceSum/allPrices.length).toFixed(2);
+    const priceAvg = (priceSum/allUsdPrices.length).toFixed(2);
     
     console.log(priceAvg)
     ;
     
     //2. Show me how to get an array of items that cost between $14.00 and $18.00 USD. Please console.log the answer.
-
-
-
+    const limitRange = items.filter(function(item){
+        return (item.price >= 14 && item.price <= 18) && item.currency_code === 'USD';
+    }).map(function(item){
+         return item.title
+        })
+    console.log(limitRange)
 
 
 
